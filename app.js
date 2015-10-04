a9ef751dd392faa7a491c3e24f212b16ef7125df
bowersad <bowersad@gmail.com>
2015-10-04 17:52:42 -0400

    Added Express.js, rearrange server.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello World');
-}).listen(3000);
+angular.module('ContactsApp',[])
+	.run(function ($rootScope) {
+		$rootScope.message = 'Hello Angular';
+	});
--- expressApp.js
+++ expressApp.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app     = express();
 
 app.get('/', function (req, res) { 
-  res.send('Hello sworld');
+  res.render('index.jade', {
+  	pageTitle : "Index"
+  });
 }).listen(3000, function () {
   console.log('you made it'); 	
 });
